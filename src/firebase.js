7c59e9aab528d7ae476685c185fb45bb42646ddd
Batmine <amine.hanifa@gmail.com>
2020-11-15 14:24:19 +0100

    Let's goIssouzone

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCVujj7nkUtO0j-szt91GnfNYn-m-4Bi7Y",
+  authDomain: "issouzone.firebaseapp.com",
+  databaseURL: "https://issouzone.firebaseio.com",
+  projectId: "issouzone",
+  storageBucket: "issouzone.appspot.com",
+  messagingSenderId: "409402167521",
+  appId: "1:409402167521:web:dbc57f18b66f6d2091a7a0",
+  measurementId: "G-3BCB34E07T",
+};
